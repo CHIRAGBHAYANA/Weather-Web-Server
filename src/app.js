badd73a38698bff11ada5c5bded137f0a9104547
CHIRAGBHAYANA <chiragbhayana2k@gmail.com>
2022-01-25 15:31:35 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require("./utils/forecast.js");
 // console.log(path.join(__dirname,'..'));
 
 const app = express();
-
+const port = process.env.PORT || 3000;
 // Define the path
 
 const publicDirectory = path.join(__dirname, "../public");
@@ -103,6 +103,6 @@ app.get("*", (req, res) => {
 //   res.send("<h1> About </h1>");
 // });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server is running up on port 3000");
 });
